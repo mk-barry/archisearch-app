a49ef9f403445ff17c747d196055fb5f8b4f0e90
mk-barry <mbatsbarry5@gmail.com>
2026-06-09 21:41:20 +0100

    sidebar repair

--- public/js/hambuger.js
+++ public/js/hambuger.js
@@ -1,27 +1,44 @@
+document.querySelectorAll('.nav-item').forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.style.display = 'none';
+        over.classList.remove('overlay');
+        main.classList.remove('disabled');
+    });
+});
+
 function sidebarToggle(){
     const sidebar = document.getElementById('main-sidebar')
     const main = document.getElementById('main')
     const over = document.getElementById('overlay')
 
-    // console.log(sidebar.style.display);
-    // sidebar.style.display = sidebar.style.display === "flex" ? "none" : "flex";
-    // console.log(sidebar.style.display);
 
     if (sidebar.style.display === "flex") {
         sidebar.style.display = "none"
-        // sidebar.style.width = "30%"
         over.classList.remove("overlay")
         main.style.marginLeft = "0"
         main.style.width = "100%"
+        main.classList.remove("disabled")
     } else {
         over.classList.add("overlay")
         sidebar.style.display = "flex"
         main.classList.add("disabled")
-        // sidebar.style.backgroundColor = "red"
-        // sidebar.style.width = "30%"
-        // main.style.width = "70%"
-        // main.style.transform = "blur(10%)"
-        // main.style.marginLeft = "30%"
     }
 
 }
+
+
+// function sidebarToggle() {
+//     const sidebar = document.getElementById('main-sidebar');
+//     const main = document.getElementById('main');
+//     const over = document.getElementById('overlay');
+
+//     if (sidebar.classList.contains('open')) {
+//         sidebar.classList.remove('open');
+//         over.classList.remove('overlay');
+//         main.classList.remove('disabled');
+//     } else {
+//         sidebar.classList.add('open');
+//         over.classList.add('overlay');
+//         main.classList.add('disabled');
+//     }
+// }
